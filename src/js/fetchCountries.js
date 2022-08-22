bb0217542c264ffaff74eebfa713edc6ecb26ffe
Mykola Kovalchuk <kolyankov@gmail.com>
2022-08-22 15:18:24 +0300

    add filter response

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,8 @@
+const BASE_URL = 'https://restcountries.com/v2';
+const FILTER_URL = '?fields=flags,name,capital,population,languages';
+
 export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v2/name/${name}`).then(response => {
+  return fetch(`${BASE_URL}/name/${name}${FILTER_URL}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
