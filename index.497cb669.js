61f3727ec170f35aecd2e65a59cbd1234be0786b
Kovalchukkk <71971559+Kovalchukkk@users.noreply.github.com>
2022-08-23 11:50:54 +0000

    Deploying to gh-pages from @ Kovalchukkk/js-hw-10@516d313239832e98b54b992fc30745276e153f01 🚀

---
+++ index.497cb669.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-box"),document.querySelector(".country-info"),document.querySelector(".country-list");
+//# sourceMappingURL=index.497cb669.js.map
---
+++ index.8284d015.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-box"),document.querySelector(".country-info"),document.querySelector(".country-list");
+//# sourceMappingURL=index.8284d015.js.map
