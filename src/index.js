d4bc3e42806f879d55390404b1416272d592cab3
Mykola Kovalchuk <kolyankov@gmail.com>
2022-08-23 14:50:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
 import getRefs from './js/get-refs';
+
 const DEBOUNCE_DELAY = 300;
 const refs = getRefs();
